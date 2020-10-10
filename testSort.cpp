2ed359139eddb95bb14e9f793389af4336ce3876
Marcelo Santana <marcelomcs@ufba.br>
2020-10-10 17:45:31 +0000

    init função getfilearr

--- testSort.cpp
+++ testSort.cpp
@@ -1,10 +1,21 @@
-#include<iostream>
-#include<string>
-#include<fstream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
+
+string getFileArray(string fileName){
+
+  
+}//pega valores do arquivo database com todas as placas
 int main(){
-  string ablue;
-  ablue="dabadidabada";
+  string myFile = string("database.txt");
+  vector<string> pivList = getFileArray(myFile)//atribui o retorno da lista de strings com o nome das placas à lista do piv, ainda desordenada
+
+  /*
+   * Aplicação do radixSort!!
+   */
+
   return 0;
 }
