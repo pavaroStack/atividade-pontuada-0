2f33997e81c54e21198fee0af976df4c03e7b829
Marcelo Santana <marcelomcs@ufba.br>
2020-10-10 20:40:22 +0000

    issue: passar vector via parametro

--- testSort.cpp
+++ testSort.cpp
@@ -5,17 +5,36 @@
 
 using namespace std;
 
-string getFileArray(string fileName){
-
-  
+string getFileArray(string fileName, (vector<string> pivList){
+  string line;
+  ifstream myfile (fileName);
+  if (myfile.is_open())
+  {
+    int i = 0;
+    string teste="test";
+    while ( getline (myfile,line) )
+    {
+      pivList.push_back(line);
+      cout << line << " " << i++ << '\n';
+    }
+    myfile.close();
+    return pivList;
+  }
+  else
+  {
+    cout<<
+          "Arquivo não abriu"
+                             <<endl;
+    return NULL;
+  }
 }//pega valores do arquivo database com todas as placas
 int main(){
-  string myFile = string("database.txt");
-  vector<string> pivList = getFileArray(myFile)//atribui o retorno da lista de strings com o nome das placas à lista do piv, ainda desordenada
+  string myFile = "database.txt";
+  vector<string> pivList = getFileArray(myFile, (vector<string>) pivList);
+
 
   /*
    * Aplicação do radixSort!!
    */
-
   return 0;
 }
